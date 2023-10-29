c01ff8b38b7d9865cb1777c2e3f586b8b3b0e088
ainemt06 <ainemt06@gmail.com>
2023-10-28 22:02:48 -0400

    bottom nav

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -61,7 +61,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
+ 
+  
   @override
+  Widget build(BuildContext context) {
+    return BottomNav();
+  }
+ 
+
+
+}
+
+
+class MenuItem {
+  const MenuItem(this.iconData, this.text);
+  final IconData iconData;
+  final String text;
+}
+
+class NavBarNotifier extends ChangeNotifier {
+  int _index = 0;
+  int get index => _index;
+  bool _hideBottomNavBar = false;
+
+  set index(int x) {
+    _index = x;
+    notifyListeners();
+  }
+}
+
+class BottomNav extends StatefulWidget {
+ const BottomNav({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavState createState() =>
+      _BottomNavState();
+}
+
+class _BottomNavState extends State {
+  int _selectedTab = 0;
+
+  final menuItemList = const <MenuItem>[
+    MenuItem(Icons.home, 'Home'),
+    MenuItem(Icons.camera, 'Scan Prescription'),
+    MenuItem(Icons.settings, 'Settings'),
+
+  ];
+
+  List _pages = [
+    Center(
+      child: Text('home page'),
+    ),
+    Center(
+      child: Text('scan page'),
+    ),
+    Center(
+      child: Text('settings page'),
+    ),
+  ];
+
+  _changeTab(int index) {
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
+   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
@@ -70,182 +136,27 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('pill id'),
-              const SizedBox(height: 10),
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const Placeholder();
-                  }));
-                },
-                icon: const Icon(Icons.camera_alt),
-                label: const Text('Take picture'),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return SurveyKit(
-                       task: manualSurvey(),
-                      showProgress: true,
-                      localizations: const {
-                        'cancel': 'Cancel',
-                        'next': 'Next',
-                      },
-                      
-                      onResult: (SurveyResult result) {
-                      
-                        final jsonResult = result.toJson();
-                        print(jsonResult);
-
-
-                    },
-                     
-                    );
-                  }));
-                },
-                icon: const Icon(Icons.add),
-                label: const Text('Add manually'),
-              ),
-            ],
-          ), // This trailing comma makes auto-formatting nicer for build methods.
-        ));
+        
+        body: _pages[_selectedTab],
+        
+        
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedTab,
+          onTap: (index) => _changeTab(index),
+        
+          elevation: 16,
+          showUnselectedLabels: true,
+          unselectedItemColor: Colors.white54,
+          selectedItemColor: Colors.white,
+          items: menuItemList.map((MenuItem menuItem) => 
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
+            icon: Icon(menuItem.iconData),
+            label: menuItem.text,
+            )).toList(),
+        )
+        );
+        
   }
-
-
-
-
-   //Data: medication name, dosage, time of day and frequency, important info
-
-    Task manualSurvey() {
-
-      var task = NavigableTask(
-        id: TaskIdentifier(),
-        steps: [
-      QuestionStep(
-        title: 'Medication Survey',
-        text: 'What is the name of the medication you are taking?',
-        answerFormat: const TextAnswerFormat(maxLines: 1),
-      ),
-
-      QuestionStep(
-        title: 'Medication Survey',
-        text: 'What is the dosage of this medication?',
-        answerFormat: const TextAnswerFormat(maxLines: 1),
-      ),
-
-      QuestionStep(
-          title: 'Medication Survey',
-          text: 'When do you take [][][][]',
-          answerFormat: const MultipleChoiceAnswerFormat(textChoices: [
-            TextChoice(text: 'Morning', value: 'Morning'),
-            TextChoice(text: 'Midday', value: 'Midday'),
-            TextChoice(text: 'Evening', value: 'Evening'),
-            TextChoice(text: 'Bedtime', value: 'Bedtime'),
-            TextChoice(text: 'Other', value: 'Other'),
-          ])),
-
-      
-
-      QuestionStep(
-        title: 'Medication Survey',
-        text: 'How much of this medication do you take in the ',
-        answerFormat: const TextAnswerFormat(maxLines: 1),
-      ),
-
-      QuestionStep(
-        title: 'Medication Survey',
-        text: 'Any other important information you\'d like to add?',
-        answerFormat: const BooleanAnswerFormat(positiveAnswer: 'Yes', negativeAnswer: 'No'),
-      ),
-
-      QuestionStep(
-        title: 'Medication Survey',
-        text: 'Please list any important directions below:',
-        answerFormat: const TextAnswerFormat(),
-      ),
-
-
-      CompletionStep(
-        stepIdentifier: StepIdentifier(id: 'what'),
-        title: 'Thank you!',
-        text: 'Your information has been saved',
-        buttonText: 'Upload to Google Calendar',
-      ),
-        ]
-    );
-    
-    task.addNavigationRule(
-      forTriggerStepIdentifier: task.steps[4].stepIdentifier,
-      navigationRule: ConditionalNavigationRule(
-        resultToStepIdentifierMapper: (input) {
-          switch (input) {
-            case 'Yes':
-              return task.steps[5].stepIdentifier;
-            case 'No':
-              return task.steps[6].stepIdentifier;
-            default:
-              return null;
-          }
-        }
-     )
-    );
-
-   /*  task.addNavigationRule(
-       forTriggerStepIdentifier: task.steps[2].stepIdentifier,
-       navigationRule: 
-    )
-    */ 
-    return task;
-    
-    }
-
-
 }
-
-/* 
-class CameraPage extends StatefulWidget {
-  final List<CameraDescription>? cameras;
-   const CameraPage({Key? key, required this.cameras});
-
-  @override
-   State<CameraPage> createState() => _CameraPageState();
-}
-
-
-class _CameraPageState extends State<CameraPage> {
-
-
-}
-
-
-
- */
