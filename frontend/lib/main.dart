d415ae07af4765db7d3aecb975baa4a891228586
Nat <patheticmustan@gmail.com>
2023-10-28 21:14:24 -0500

    clean up

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-import'dart:convert';
-import 'package:survey_kit/survey_kit.dart';
-//import 'package:flutter_camera_practice/preview_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -11,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -42,37 +22,18 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
-  //final List<String> results;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
- 
-  
   @override
   Widget build(BuildContext context) {
-    return BottomNav();
+    return const BottomNav();
   }
- 
-
-
 }
-
 
 class MenuItem {
   const MenuItem(this.iconData, this.text);
@@ -83,7 +44,7 @@ class MenuItem {
 class NavBarNotifier extends ChangeNotifier {
   int _index = 0;
   int get index => _index;
-  bool _hideBottomNavBar = false;
+  final bool _hideBottomNavBar = false;
 
   set index(int x) {
     _index = x;
@@ -92,31 +53,29 @@ class NavBarNotifier extends ChangeNotifier {
 }
 
 class BottomNav extends StatefulWidget {
- const BottomNav({Key? key}) : super(key: key);
+  const BottomNav({super.key});
 
   @override
-  _BottomNavState createState() =>
-      _BottomNavState();
+  State<BottomNav> createState() => _BottomNavState();
 }
 
-class _BottomNavState extends State {
+class _BottomNavState extends State<BottomNav> {
   int _selectedTab = 0;
 
   final menuItemList = const <MenuItem>[
     MenuItem(Icons.home, 'Home'),
     MenuItem(Icons.camera, 'Scan Prescription'),
     MenuItem(Icons.settings, 'Settings'),
-
   ];
 
-  List _pages = [
-    Center(
+  final List _pages = [
+    const Center(
       child: Text('home page'),
     ),
-    Center(
+    const Center(
       child: Text('scan page'),
     ),
-    Center(
+    const Center(
       child: Text('settings page'),
     ),
   ];
@@ -127,7 +86,7 @@ class _BottomNavState extends State {
     });
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
@@ -136,27 +95,22 @@ class _BottomNavState extends State {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        
         body: _pages[_selectedTab],
-        
-        
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.shifting,
           currentIndex: _selectedTab,
           onTap: (index) => _changeTab(index),
-        
           elevation: 16,
           showUnselectedLabels: true,
           unselectedItemColor: Colors.white54,
           selectedItemColor: Colors.white,
-          items: menuItemList.map((MenuItem menuItem) => 
-          BottomNavigationBarItem(
-            backgroundColor: Colors.blue,
-            icon: Icon(menuItem.iconData),
-            label: menuItem.text,
-            )).toList(),
-        )
-        );
-        
+          items: menuItemList
+              .map((MenuItem menuItem) => BottomNavigationBarItem(
+                    backgroundColor: Colors.blue,
+                    icon: Icon(menuItem.iconData),
+                    label: menuItem.text,
+                  ))
+              .toList(),
+        ));
   }
 }
